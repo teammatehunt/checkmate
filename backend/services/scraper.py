164f94ed7e3454d85a18c524e0b216cf5aed83e5
Brian Shimanuki <brianshimanuki@gmail.com>
2021-01-15 18:39:14 +0000

    scraper

--- backend/services/scraper.py
+++ backend/services/scraper.py
@@ -39,11 +39,17 @@ class Client:
     async def login(self):
         login_page = self.bot_config.login_page
         headers = {}
+        csrftoken = None
         if login_page:
-            headers['Referrer'] = login_page
+            headers['Referer'] = login_page
+            async with self.session.get(login_page) as resp:
+                data = await resp.read()
+            soup = BeautifulSoup(data, 'html5lib')
+            csrftoken = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get('value')
         payload = {
-            'username': settings.SECRETS['LOGIN']['username'],
-            'password': settings.SECRETS['LOGIN']['password'],
+            'team': settings.SECRETS['LOGIN']['username'],
+            'pass': settings.SECRETS['LOGIN']['password'],
+            'csrfmiddlewaretoken': csrftoken,
         }
         login_api = self.bot_config.login_api_endpoint
         if not login_api:
@@ -67,10 +73,8 @@ class Client:
             return await resp.read()
 
     async def fetch(self):
+        await self.login()
         data = await self.try_fetch()
-        if data is None:
-            await self.login()
-            data = await self.try_fetch()
         if data is None:
             raise RuntimeError('Could not fetch puzzles page')
         return data
@@ -90,10 +94,10 @@ async def fetch_site_data():
     client = Client(bot_config)
     data = await client.fetch()
     json_data = None
-    try:
-        json_data = json.loads(data)
-    except:
-        pass
+    # try:
+        # json_data = json.loads(data)
+    # except:
+        # pass
     if json_data is not None:
         return parse_json(data)
     soup = None
@@ -130,4 +134,29 @@ def parse_json(data):
     return scraper_examples.parse_json_mh19(data)
 
 def parse_html(soup: BeautifulSoup):
-    return scraper_examples.parse_html_mh20(soup)
+    return parse_html_mh21(soup)
+
+def parse_html_mh21(soup):
+    # MH 2020
+    results = defaultdict(list)
+    main = soup.find('main')
+    for section in main.find_all('section'):
+        a_round = section.a
+        round_name = a_round.h3.string
+        results['rounds'].append({
+            'name': round_name,
+            'link': a_round.get('href'),
+        })
+        for tr_puzzle in section.find_all('tr'):
+            tds = tr_puzzle.find_all('td')
+            if tds:
+                answer = tds[1].string.strip()
+                puzzle = {
+                    'name': tds[0].a.string.strip(),
+                    'link': tds[0].a.get('href'),
+                    'round_names': [round_name],
+                    'answer': answer,
+                    'is_solved': bool(answer),
+                }
+                results['puzzles'].append(puzzle)
+    return results
